f44a50953303fb0a0be55c43f23806952408afa6
Leandro Costa <lcosta200212@gmail.com>
2023-12-11 18:26:46 -0300

    feat: criado repository da entidade hotel

--- api/viagemais/src/main/java/com/leandro/viagemais/repositories/HotelRepository.java
+++ api/viagemais/src/main/java/com/leandro/viagemais/repositories/HotelRepository.java
@@ -3,9 +3,11 @@ package com.leandro.viagemais.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.leandro.viagemais.entities.Hotel;
 
+@Repository
 public interface HotelRepository extends JpaRepository<Hotel, UUID> {
 
 }
