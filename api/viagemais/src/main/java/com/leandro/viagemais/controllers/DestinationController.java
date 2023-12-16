a31355b867b119e224a6ccc6faac1930a4041061
Leandro Costa <lcosta200212@gmail.com>
2023-12-16 15:29:15 -0300

    feat: controller passa a usar os metodos do controller generico

--- api/viagemais/src/main/java/com/leandro/viagemais/controllers/DestinationController.java
+++ api/viagemais/src/main/java/com/leandro/viagemais/controllers/DestinationController.java
@@ -1,74 +1,21 @@
 package com.leandro.viagemais.controllers;
 
-import java.util.List;
 import java.util.UUID;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.leandro.viagemais.dto.DestinationDTO;
 import com.leandro.viagemais.entities.Destination;
 import com.leandro.viagemais.services.DestinationService;
 
-import jakarta.validation.Valid;
-
 @RestController
 @RequestMapping("api/destination")
-public class DestinationController {
+public class DestinationController extends GenericController<Destination, UUID, DestinationService> {
 
-  private DestinationService destinationService;
+  public DestinationController(@Qualifier("destinationService") DestinationService service) {
+    super(service);
 
-  public DestinationController(DestinationService destinationService) {
-    this.destinationService = destinationService;
-  }
-
-  @GetMapping
-  public ResponseEntity<List<Destination>> list() {
-    var destinations = destinationService.findAll();
-
-    return ResponseEntity.ok().body(destinations);
-  }
-
-  @PostMapping
-  public ResponseEntity<String> create(@RequestBody Destination data) {
-    // var newDestination = new Destination(data);
-
-    // this.destinationService.save(newDestination);
-    this.destinationService.save(data);
-
-    return ResponseEntity.status(201).build();
-  }
-
-  @GetMapping("/{id}")
-  public ResponseEntity<Destination> findById(@PathVariable("id") UUID id) {
-    var destination = this.destinationService.findById(id);
-
-    if (destination.isPresent()) {
-      return ResponseEntity.ok().body(destination.get());
-    }
-
-    return ResponseEntity.noContent().build();
-  }
-
-  @PutMapping("/{id}")
-  public ResponseEntity<String> update(@PathVariable("id") UUID id, @RequestBody @Valid DestinationDTO data) {
-
-    return ResponseEntity.ok().build();
-
-  }
-
-  @DeleteMapping("/{id}")
-  public ResponseEntity<String> delete(@PathVariable("id") UUID id) {
-    this.destinationService.deleteById(id);
-
-    return ResponseEntity.noContent().build();
   }
 
 }
--- api/viagemais/src/main/java/com/leandro/viagemais/controllers/HotelController.java
+++ api/viagemais/src/main/java/com/leandro/viagemais/controllers/HotelController.java
@@ -1,71 +1,22 @@
 package com.leandro.viagemais.controllers;
 
-import java.util.List;
 import java.util.UUID;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leandro.viagemais.entities.Hotel;
 import com.leandro.viagemais.services.HotelService;
 
-import jakarta.validation.Valid;
-
 @RestController
 @RequestMapping("api/hotel")
-public class HotelController {
+public class HotelController extends GenericController<Hotel, UUID, HotelService> {
 
-  private HotelService hotelService;
+  public HotelController(@Qualifier("hotelService") HotelService service) {
+    super(service);
 
-  public HotelController(HotelService hotelService) {
-    this.hotelService = hotelService;
-  }
-
-  @GetMapping
-  public ResponseEntity<List<Hotel>> list() {
-    var hotels = this.hotelService.findAll();
-
-    return ResponseEntity.ok().body(hotels);
-  }
-
-  @PostMapping
-  public ResponseEntity<String> create(@RequestBody @Valid Hotel data) {
-
-    this.hotelService.save(data);
-
-    return ResponseEntity.status(201).build();
-  }
-
-  @GetMapping("/{id}")
-  public ResponseEntity<Hotel> findById(@PathVariable("id") UUID id) {
-    var hotel = this.hotelService.findById(id);
-
-    if (hotel.isPresent()) {
-      return ResponseEntity.ok().body(hotel.get());
-    }
-
-    return ResponseEntity.noContent().build();
-  }
-
-  @PutMapping("/{id}")
-  public ResponseEntity<String> update(@PathVariable("id") UUID id, @RequestBody @Valid Hotel data) {
-
-    return ResponseEntity.ok().build();
-
-  }
-
-  @DeleteMapping("/{id}")
-  public ResponseEntity<String> delete(@PathVariable("id") UUID id) {
-    this.hotelService.deleteById(id);
-
-    return ResponseEntity.noContent().build();
   }
 
 }
