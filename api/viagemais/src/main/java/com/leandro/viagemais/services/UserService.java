9df707cd6739d725916531a5b0af771e5f3fb180
Leandro Costa <lcosta200212@gmail.com>
2023-12-16 15:41:07 -0300

    removido metodo sem uso

--- api/viagemais/src/main/java/com/leandro/viagemais/services/UserService.java
+++ api/viagemais/src/main/java/com/leandro/viagemais/services/UserService.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import com.leandro.viagemais.dto.UserTicketDTO;
 import com.leandro.viagemais.entities.user.User;
 import com.leandro.viagemais.repositories.UserRepository;
 import com.leandro.viagemais.servicesImpl.ServiceImplGeneric;
@@ -15,12 +14,5 @@ public class UserService extends ServiceImplGeneric<User, UUID, UserRepository> 
 
   public UserService(@Qualifier("userRepository") UserRepository repository) {
     super(repository);
-
   }
-
-  public void addUserTicket(UserTicketDTO data) {
-    this.repository.addUserTicket(data.userId(), data.ticketId());
-
-  }
-
 }
