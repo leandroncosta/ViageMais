1c307f560f44f68e789dd0686bd7a08eb86237f5
Leandro Costa <lcosta200212@gmail.com>
2023-12-16 15:08:40 -0300

    test: sem uso no momento

--- api/viagemais/src/test/java/com/leandro/viagemais/ViagemaisApplicationTests.java
+++ api/viagemais/src/test/java/com/leandro/viagemais/ViagemaisApplicationTests.java
@@ -1,13 +1,48 @@
 package com.leandro.viagemais;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+// @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ViagemaisApplicationTests {
 
+	// @Autowired
+	// private WebTestClient webTestClient;
+
+	// @Test
+	// void registerSucess() {
+	// var user = new User("ncosta", "1234", UserRole.ADMIN);
+
+	// webTestClient
+	// .post()
+	// .uri("/auth/register")
+	// .bodyValue(user)
+	// .exchange()
+	// .expectStatus().isOk();
+
+	// }
+
+	// @Test
+	// void loginSucess() {
+	// var user = new User();
+	// user.setLogin("ncosta");
+	// user.setPassword("1234");
+
+	// webTestClient
+	// .post()
+	// .uri("/auth/login")
+	// .bodyValue(user)
+	// .exchange()
+	// .expectStatus().isOk()
+	// .expectBody()
+	// .jsonPath("$.token");
+
+	// }
+
 	@Test
-	void contextLoads() {
+	void testCreateDestinationFailure() {
 	}
 
 }
