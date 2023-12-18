a3d4a15618441415747ac79d7d9aa4108602526b
Leandro Costa <lcosta200212@gmail.com>
2023-12-17 23:04:33 -0300

    feat: envolvendo o context em todas as paginas

--- client/viage-mais/src/Components/MainContainer.js
+++ client/viage-mais/src/Components/MainContainer.js
@@ -1,11 +1,12 @@
+import { UserStorage } from '@/store/userContext';
 import Header from './Header';
 
 const MainContainer = ({ children }) => {
     return (
-        <>
+        <UserStorage>
             <Header />
             {children}
-        </>
+        </UserStorage>
     );
 };
 
--- client/viage-mais/src/pages/_app.js
+++ client/viage-mais/src/pages/_app.js
@@ -1,11 +1,11 @@
-import MainContainer from '@/Components/MainContainer'
-import '@/styles/globals.css'
-import '@/styles/main.min.css'
+import MainContainer from '@/Components/MainContainer';
+import '@/styles/globals.css';
+import '@/styles/main.min.css';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <MainContainer>
-      <Component {...pageProps} />
-    </MainContainer>
-  )
+    return (
+        <MainContainer>
+            <Component {...pageProps} />
+        </MainContainer>
+    );
 }
