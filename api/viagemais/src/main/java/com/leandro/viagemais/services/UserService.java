48bb3e02fa1c5600b1dd1ea790c6aadac38cb49f
Leandro Costa <lcosta200212@gmail.com>
2023-12-17 21:34:39 -0300

    feat:  metodo para verificar informacoes de um usuario autenticado

--- api/viagemais/src/main/java/com/leandro/viagemais/services/UserService.java
+++ api/viagemais/src/main/java/com/leandro/viagemais/services/UserService.java
@@ -3,6 +3,8 @@ package com.leandro.viagemais.services;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.leandro.viagemais.entities.user.User;
@@ -14,5 +16,24 @@ public class UserService extends ServiceImplGeneric<User, UUID, UserRepository> 
 
   public UserService(@Qualifier("userRepository") UserRepository repository) {
     super(repository);
+  }
+
+  public UserDetails findByLogin(String login) {
+    var user = this.repository.findByLogin(login);
+
+    return user;
+  }
+
+  public UserDetails getUserInfo() {
+    // Obtém a autenticação do contexto de segurança
+    var authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    // Verifica se o usuário está autenticado
+    if (authentication.isAuthenticated() && authentication.getPrincipal() instanceof UserDetails) {
+      // Extrai os detalhes do usuário a partir do principal
+      UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+      return userDetails;
+    }
+    return null;
   }
 }
